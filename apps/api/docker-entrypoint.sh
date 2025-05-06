d273ece8172b2a2c5d719457259698d0ac504b53
mayeulrougevin <contact@mayeulrougevin.fr>
2025-05-06 05:51:12 +0200

    all

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -1,24 +1,25 @@
 #!/bin/bash -e
 
+# Affiche l'utilisateur courant
 if [ "$UID" -eq 0 ]; then
-  set +e # disable failing on errror
+  echo "ENTRYPOINT is running as root"
   ulimit -n 65535
   echo "NEW ULIMIT: $(ulimit -n)"
-  set -e # enable failing on error
 else
-  echo ENTRYPOINT DID NOT RUN AS ROOT
+  echo "ENTRYPOINT did not run as root"
 fi
 
-if [ "$FLY_PROCESS_GROUP" = "app" ]; then
-  echo "RUNNING app"
+# Choix du processus selon la variable PROCESS_TYPE
+if [ "$PROCESS_TYPE" = "api" ]; then
+  echo "RUNNING Firecrawl API"
   node --max-old-space-size=8192 dist/src/index.js
-elif [ "$FLY_PROCESS_GROUP" = "worker" ]; then
-  echo "RUNNING worker"
+elif [ "$PROCESS_TYPE" = "worker" ]; then
+  echo "RUNNING Firecrawl Worker"
   node --max-old-space-size=8192 dist/src/services/queue-worker.js
-elif [ "$FLY_PROCESS_GROUP" = "index-worker" ]; then
-  echo "RUNNING index worker"
+elif [ "$PROCESS_TYPE" = "index-worker" ]; then
+  echo "RUNNING Indexing Worker"
   node --max-old-space-size=8192 dist/src/services/indexing/index-worker.js
 else
-  echo "NO FLY PROCESS GROUP"
+  echo "NO VALID PROCESS_TYPE PROVIDED - running default API"
   node --max-old-space-size=8192 dist/src/index.js
 fi
